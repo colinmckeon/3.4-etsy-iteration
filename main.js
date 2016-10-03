d384924bcec98078e32cce1a9bac0f336a2935be
Colin McKeon <14mckeon@gmail.com>
2016-10-03 17:52:28 -0400

    cleaned up code

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 
 /*******************************************
 QUESTION #1
- *******************************************/
+*******************************************/
 
 console.log(items.price);
 
@@ -22,53 +22,16 @@ console.log('The average price is $' + (itemTotal/items.length).toFixed(2) );
 
 /*******************************************
 QUESTION #2
- *******************************************/
-
-var costList = items.filter(function(item){
-  return item.price >14 && item.price <18;
-
-});
-console.log("Items that cost between $14.00 USD and $18.00 USD:");
-console.log(costList);
-
-
-
-
-
-////////// THANK YOU JOEL!
-
-// alert ("colin");
-
-(function(){
-  "use strict"
-
-
-/*******************************************
-QUESTION #1
 *******************************************/
 
-// console.log(items.price);
-//
-// var itemTotal = items.reduce(function(total, item){
-//
-//     console.log(item.price);
-//     return total + item.price;
-// }, 0);
-//
-// console.log('The average price is $' + (itemTotal/items.length).toFixed(2) );
-//
-//
+ var cost = items.filter(function(item) {
+   return (item.price < 18 && item.price > 14)
 
-/*******************************************
-QUESTION #2
-*******************************************/
+ });
+ console.log(cost);
+ console.log("Items that cost between $14.00 USD and $18.00 USD:");
 
- // var cost = items.filter(function(item) {
- //   return (item.price < 18 && item.price > 14)
- //
- // });
- // console.log(cost);
- //
+
 
 
 
@@ -76,60 +39,60 @@ QUESTION #2
  QUESTION #3
 *******************************************/
 
-//
-// var quid = items.filter(function(product) {
-//   if(product.currency_code == 'GBP'){
-//     return true;
-//   } else {
-//     return false;
-//   }
-// });
-//
-// // console.log(quid[0].title);
-//
-// quid.forEach(function(product) {
-//   console.log(product.title);
-// })
-//
+
+var quid = items.filter(function(product) {
+  if(product.currency_code == 'GBP'){
+    return true;
+  } else {
+    return false;
+  }
+});
+
+// console.log(quid[0].title);
+
+quid.forEach(function(product) {
+  console.log(product.title);
+})
+
 
 
 /*******************************************
  QUESTION #4
  *******************************************/
 
-// var woodenItems = items.filter(function(product) {
-//   if(product.materials.includes('wood')){
-//     return true;
-//   } else {
-//     return false;
-//   }
-// });
-//
-// woodenItems.forEach(function(item) {
-//   console.log(item.title);
-// });
-//
-// console.log(woodenItems[0]);
+var woodenItems = items.filter(function(product) {
+  if(product.materials.includes('wood')){
+    return true;
+  } else {
+    return false;
+  }
+});
+
+woodenItems.forEach(function(item) {
+  console.log(item.title);
+});
+
+console.log(woodenItems[0]);
 
 
 /*******************************************
  QUESTION #5
 *******************************************/
 
-  // var largeList = items.filter(function(item) {
-  //   return (item.materials.length >= 8)
-  //
-  // });
-  // largeList.forEach(function(item){
-  //   console.log(item.title);
-  //   var materials = item.materials;
-  //   materials.forEach(function(item){
-  //     console.log(item);
-  //   })
-  //
-  //
-  // });
-  //
+  var largeList = items.filter(function(item) {
+    return (item.materials.length >= 8)
+
+  });
+  largeList.forEach(function(item){
+    console.log(item.title);
+    var materials = item.materials;
+    materials.forEach(function(item){
+      console.log(item);
+    })
+
+
+  });
+
 
 
 
@@ -148,9 +111,3 @@ console.log(iMade.length + ' were made by their sellers');
 
 
 }()); // END OF GLOBAL FUNCTION
-
-
-
-
-
- }());
